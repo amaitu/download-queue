e1c2d380e05baa7ba6cb953546a0e9f66bb2aaaa
amaitu <steve@sgbarker.com>
2020-04-02 22:31:22 +0100

    All downloads endpoint.

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return {"body": "it wurks"}
+@app.route("/downloads/all")
+def get_all_downloads():
+    # INSERT
+    # INTO
+    # "jobs"("id", "created_at", "processed", "url", "dir", "filename")
+    # VALUES
+    # ('7d2e4aaa-5901-46aa-82ad-8950e839078b', '2020-01-01 00:00:00', '0', 'https://sgbarker.com', 'lund', 'lund.csv');
+
+    conn = sqlite3.connect("database.sqlite")
+    c = conn.cursor()
+
+    results = {}
+    for row in c.execute(
+        "SELECT id, created_at, processed, url, dir, filename FROM jobs"
+    ):
+        result_id = row[0]
+        result = {
+            "id": result_id,
+            "created_at": row[1],
+            "processed": row[2],
+            "url": row[3],
+            "dir": row[4],
+            "filename": row[5],
+        }
+
+        results[result_id] = result
+
+    return results
 
 
 if __name__ == "__main__":
