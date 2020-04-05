3e35004e2f59427857e09ebcac624b52783fc1d8
amaitu <steve@sgbarker.com>
2020-04-05 19:00:51 +0100

    Accept processed param

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import request
 import sqlite3
 
 app = Flask(__name__)
 
 
-@app.route("/downloads/all")
+@app.route("/downloads/all", methods=["GET"])
 def get_all_downloads():
     # INSERT
     # INTO
@@ -12,13 +13,31 @@ def get_all_downloads():
     # VALUES
     # ('7d2e4aaa-5901-46aa-82ad-8950e839078b', '2020-01-01 00:00:00', '0', 'https://sgbarker.com', 'lund', 'lund.csv');
 
+    processed = None
+
+    if request.args.get("processed") == "true":
+        processed = "1"
+
+    if request.args.get("processed") == "false":
+        processed = "0"
+
     conn = sqlite3.connect("database.sqlite")
     c = conn.cursor()
 
     results = {}
-    for row in c.execute(
-        "SELECT id, created_at, processed, url, dir, filename FROM jobs"
-    ):
+
+    if processed is None:
+        query = c.execute(
+            "SELECT id, created_at, processed, url, dir, filename FROM jobs ORDER BY created_at DESC LIMIT 250"
+        )
+    else:
+        print(processed)
+        query = c.execute(
+            "SELECT id, created_at, processed, url, dir, filename FROM jobs WHERE processed = ? ORDER BY created_at DESC LIMIT 250",
+            processed,
+        )
+
+    for row in query:
         result_id = row[0]
         result = {
             "id": result_id,
